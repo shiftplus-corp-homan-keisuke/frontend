6e6cc9df231b0c2027f773c5edd6a737e78e3678
keisuke homan <hiki30@gmail.com>
2025-08-25 00:17:43 +0900

    Add introductory session on the necessity of frontend frameworks, focusing on React

---
+++ react/STEP01_基礎/session2_project/starter/data.js
@@ -0,0 +1,56 @@
+const pizzaData = [
+  {
+    name: "フォカッチャ",
+    ingredients: "イタリアンオリーブオイルとローズマリーのパン",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "マルゲリータピザ",
+    ingredients: "トマトとモッツァレラチーズ",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "スピナーチピザ",
+    ingredients: "トマト、モッツァレラ、ほうれん草、リコッタチーズ",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "フンギピザ",
+    ingredients: "トマト、モッツァレラ、マッシュルーム、玉ねぎ",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "サラミーノピザ",
+    ingredients: "トマト、モッツァレラ、ペパロニ",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "プロシュート・ディ・パルマピザ",
+    ingredients: "トマト、モッツァレラ、ハム、ルッコラ、ブッラータチーズ",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return <div>ピザ</div>;
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
